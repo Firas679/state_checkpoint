ec2cfb9cdaa3e0ca032bb04778cc8c1ebd39ea31
Firas679 <essidfiras679@gmail.com>
2023-01-24 09:50:59 +0100

    fixing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React from "react";
-import Counter from './Component/Counter';
+import Counter from "./Component/Counter";
 
 class App extends React.Component {
+  constructor() {
+    super();
 
-  constructor(){
-    super()
-
-    this.state= {
-        name:'Firas',
-        Bio:'Student At GoMyCode',
-        count:0,
-        show: false
-    }
+    this.state = {
+      name: "Firas",
+      Bio: "Student At GoMyCode",
+      count: 0,
+      show: false,
+    };
   }
-  toggleShow =()=>{
-    this.setState({show: !this.state.show})
-  }
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
 
-  render () {
-    let Bio = this.state.Bio
+  render() {
+    let Bio = this.state.Bio;
     return (
-      <div className="App">
-       <h3> My name's' {this.state.name} </h3>
-       <p>{Bio}</p>
-       <h3> Counter </h3>
-       <button onClick={this.toggleShow}> Click To see some MAGIC</button>
-        {this.state.show && <Counter/>}
+      <div>
+        <div className="App">
+          { this.state.show  &&
+            <>
+              <h3> My name's' {this.state.name} </h3>
+              <p>{Bio}</p>
+              <Counter />
+            </>
+          }
+          <h3> Counter </h3>
+        </div>
+          <button onClick={this.toggleShow}> Click To see some MAGIC</button>
       </div>
-    )
+    );
   }
 }
 
